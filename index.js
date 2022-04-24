da7bf7aab179f6e4afe9ab561e10f6813d5998bb
saghar <behinaeen.saghar@gmail.com>
2022-04-24 08:09:05 -0700

    options

--- index.js
+++ index.js
@@ -1,85 +1,115 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
-let employeeInfo=[];
+let employeeInfo = [];
 
 
 
 
 // TODO: Enter the team manager’s name, employee ID, email address, and office number
-const init= async function () {
-    try{
-        const managerInfo= await managerinfo();
-    }catch{
+const init = async function () {
+    try {
+        const generalAnswer = await getGeneralInfo();
+        const managerInfo = await getManagerInfo();
+        let newManager = new Manager(generalAnswer.emplyeeName, generalAnswer.employeeID, generalAnswer.employeeEmail, managerInfo.managerOfficeNum)
+        employeeInfo.push(newManager);
+        managerOptions();
+        //  creatHTMLfile()
+    } catch {
 
     }
 
 }
-const managerinfo= async function () {
-   const generalAnswer= await getGeneralInfo();
-   inquirer.prompt([
-    {
-         type: 'number',
-         name: 'managerOfficeNum',
-         message: 'please enter Manager Office number'
-     }
+function getManagerInfo() {
+    return inquirer.prompt([
+        {
+            type: 'number',
+            name: 'managerOfficeNum',
+            message: 'please enter Manager Office number'
+        }
     ])
-    .then((answers) => {
-        let newManager = new Manager(generalAnswer.employeeName, generalAnswer.employeeID, generalAnswer.employeeEmail, answers.managerOfficeNum)
-       employeeInfo.push(newManager);
-        managerOptions();
 
-    })
 }
-function managerOptions() {
-    inquirer.prompt([
+const managerOptions = async function () {
+    const opt = await inquirer.prompt([
         {
             type: 'list',
             name: 'employeeOptions',
-            message: 'please enter Manager Office number',
+            message: 'please select an Employee',
             choices: ['Engineer', 'Inter', 'finish building my team']
         }
         // TODO: presented with a menu with the option to add an engineer
         //  or an intern or to finish building my team
-    ]).then((answers) => {
-        switch (answers.employeeOptions) {
-            // TODO: select the engineer option
-            case 'Engineer':
-                EngineerQ();
-                break;
-            case 'Inter':
+    ])
 
-                break;
-            case 'finish building my team':
+    switch (opt.employeeOptions) {
+        // TODO: select the engineer option
+        case 'Engineer':
+            console.log("//////Engineer Information//////")
+            const generalAnswerM = await getGeneralInfo();
+            const enginerrAns = await getEngineerlInfo();
+            employeeInfo.push(new Engineer(generalAnswerM.emplyeeName, generalAnswerM.employeeID,
+                generalAnswerM.employeeEmail, enginerrAns.github))
 
-                break;
+            managerOptions();
+            break;
+        // TODO: select the intern option
+        case 'Inter':
+            console.log("//////Intern Information//////")
+            const generalAnswerIn = await getGeneralInfo();
+            const internAns = await getInternlInfo();
+            employeeInfo.push(new Intern(generalAnswerIn.emplyeeName, generalAnswerIn.employeeID,
+                generalAnswerIn.employeeEmail, internAns.school))
+            managerOptions();
 
-            default:
-                
-                break;
-        }
+            break;
+        // TODO: exit the application, and the HTML is generated
+        case 'finish building my team':
+            console.log("your team is:")
+            console.table(employeeInfo)
+            creatHTMLfile()
+            break;
 
-    })
-}
-function engineerQ() {
-inquirer.prompt([
-    {
-        
+        default:
+            console.table(employeeInfo)
+            break;
     }
 
-])
+
 }
-const getManagerlInfo =function(){
-   return inquirer.prompt([
+const getEngineerlInfo = function () {
+    return inquirer.prompt([
         {
-             type: 'number',
-             name: 'managerOfficeNum',
-             message: 'please enter Manager Office number'
-         }
-        ])
-    }
-const getGeneralInfo =  function (){
-   return  inquirer.prompt([
+            type: 'input',
+            name: 'github',
+            message: 'please enter Engineer GitHub username'
+        }
+    ])
+
+}
+const getInternlInfo = function () {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'school',
+            message: 'please enter Intern school name'
+        }
+    ])
+
+}
+const getManagerlInfo = function () {
+    return inquirer.prompt([
+        {
+            type: 'number',
+            name: 'managerOfficeNum',
+            message: 'please enter Manager Office number'
+        }
+    ])
+}
+const getGeneralInfo = function () {
+    return inquirer.prompt([
         {
             type: 'input',
             message: 'please enter name',
@@ -99,13 +129,25 @@ const getGeneralInfo =  function (){
 }
 init()
 
+function creatHTMLfile() {
+    saveEmployees();
+ let employeeList=readEmployees();
+const htmlfile = htmlFile(employeeInfo);
+    console.log("creat html file")
+}
 
-// TODO: prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
-// TODO: select the intern option
-// TODO: prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
-// TODO: decide to finish building my team
-// TODO: exit the application, and the HTML is generated
+function saveEmployees(){
 
+}
+function readEmployees(){
+    const employeelist=[];
+return employeelist
+}
+function htmlFile(employees){
 // TODO: click on an email address in the HTMLTHEN my default 
 //       email program opens and populates the TO field of the email with the address
 // TODO: click on the GitHub username THEN that GitHub profile opens in a new tab
+
+}
+
+
